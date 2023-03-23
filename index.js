35faca99798f0607744bd8563f6b8fd75ce40a2e
Brian <mahuibrian14@gmail.com>
2023-03-23 09:24:33 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+  const distanceFromHqInBlocks=function(destination,location){
+    const location =42;
+    return (destination-location);
+ }
+ const distanceFromHqInFeet=function(distanceFromHqInBlocks){
+    return (distanceFromHqInBlocks*264);
+    
+ }
+ const distanceTravelledInFeet = function(start,destination){
+    return ((destination-start)*264);
+ }
+function calculatesFarePrice(start,destination){
+    
+    if((start-destination)=distanceTravelledInFeet<400){
+        return 'gives customers a free sample';
+    }
+    if((start-destination)=distanceTravelledInFeet>=400 && distanceTravelledInFeet<=2000){
+        return `${(2*distanceTravelledInFeet)} cents`;
+    }
+    if((start-destination)=distanceTravelledInFeet>2000 && distanceTravelledInFeet<2500) {
+        return '25 dollars';
+    }
+    if((start-destination)=distanceTravelledInFeet>2500) {
+        return 'cannot travel that far';
+    }
+}
+     
+    
+    
+    
+   
+ 
